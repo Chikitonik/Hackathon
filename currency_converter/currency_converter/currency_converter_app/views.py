2155c4843e1b0df98640494799312b0f321f610d
Chikitonik <chikitonik@gmail.com>
2023-09-04 22:57:55 +0300

    currency_converter v2

--- currency_converter/currency_converter/currency_converter_app/views.py
+++ currency_converter/currency_converter/currency_converter_app/views.py
@@ -1,12 +1,8 @@
 import datetime
 import os
-import matplotlib.pyplot as plt
 from django.shortcuts import render
 from .models import Currency
 import requests
-import pandas as pd
-import matplotlib
-matplotlib.use('Agg')  # Use the Agg backend (for non-GUI environments)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 URL_API_EXCHANGE = 'https://api.exchangerate.host/timeseries'
@@ -31,11 +27,13 @@ def get_currency_history(from_currency, to_currency, amount, start_date, end_dat
         data = response.json()
         currency_history = {}
         for date, rates in data['rates'].items():
+            # Ensure date is a datetime.date object
+            date_obj = datetime.datetime.strptime(date, '%Y-%m-%d').date()
             if to_currency in rates:
-                currency_history[date] = rates[to_currency]
+                currency_history[date_obj] = rates[to_currency]
             else:
                 # Handle the case where the rate is missing
-                currency_history[date] = None
+                currency_history[date_obj] = None
         return currency_history
     except Exception as e:
         print(f'Error fetching data: {e}')
@@ -43,7 +41,7 @@ def get_currency_history(from_currency, to_currency, amount, start_date, end_dat
 
 
 def currency_converter_view(request, from_currency, amount, percent, to_currency, period):
-    context = {}
+
     # Data collecting
     amount = float(amount)
     percent = float(percent)
@@ -62,49 +60,29 @@ def currency_converter_view(request, from_currency, amount, percent, to_currency
         # default 1 month
         start_date = end_date - datetime.timedelta(days=30)
 
-    # Get data for each year in the selected period
-    currency_history = {}
+    # Get data for each year in the selected period (API allows just 1 year query)
+    rate_dates = []
+    rate_values = []
     while start_date <= end_date:
         year_end_date = start_date + datetime.timedelta(days=365)
         if year_end_date > end_date:
             year_end_date = end_date
+
         year_currency_history = get_currency_history(
             from_currency, to_currency, amount, start_date, year_end_date)
-        currency_history.update(year_currency_history)
+        for date, rate in year_currency_history.items():
+            parsed_date = date.strftime("%Y-%m-%d")  # Convert date to string
+            rate_dates.append(parsed_date)
+            rate_values.append(rate)
+
         start_date = year_end_date + datetime.timedelta(days=1)
 
-    pd_currency_history = pd.DataFrame(
-        {'Date': list(currency_history.keys()), 'Value': list(currency_history.values())})
-    pd_currency_history.set_index('Date', inplace=True)
-    pd_currency_history.columns = ['Rate']
-    # Plotting
-    # plt.figure(figsize=(10, 6))
-    plt.plot(pd_currency_history.index, pd_currency_history['Rate'])
-    # plt.ylabel(f'{amount} {from_currency} to {to_currency}')
-    # plt.xlabel('Date')
-    # plt.title('Currency Exchange Rate Over Time')
-    # plt.xticks(rotation=45)
-    # plt.tight_layout()
-
-    # Save the plot as an image
-    plot_image_name = 'currency_plot.png'  # Name of the image file
-    plot_image_path = os.path.join(
-        BASE_DIR, 'currency_converter_app', 'static', plot_image_name)
-
-    plt.savefig(plot_image_path)
-
-    plot_image_path = 'currency_plot.png'
-    plt.savefig(plot_image_path)
-
-    # Close the plot to release resources
-    plt.close()
-
-    # Pass the image path to the template
-    context['plot_image_path'] = plot_image_path
-
     currencies = Currency.objects.all()
-    context['currencies'] = currencies
-
-    # currencies = Currency.objects.all()
-    # context = {'currencies': currencies}
-    return render(request, 'currency_converter.html', context)
+    return render(request, 'currency_converter.html',
+                  {'amount': amount,
+                   'percent': percent,
+                   'from_currency': from_currency,
+                   'to_currency': to_currency,
+                   'currencies': currencies,
+                   'period': period,
+                   'rate_dates': rate_dates, 'rate_values': rate_values})
